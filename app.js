42a4c2852235a8fcc46526abcecac1a7f14d23fe
Johnny Bieniek <jjbieniek@gmail.com>
2023-01-23 11:58:59 -0500

    base project finished

--- app.js
+++ app.js
@@ -3,30 +3,41 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+let items = [];
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  var today = new Date();
-  var currentDay = today.getDay();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItem: items });
+  res.render("list", { listTitle: day, newListItem: items });
 });
 
 app.post("/", (req, res) => {
-  item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItem: workItems });
 });
 
 app.listen(3000, () => {
